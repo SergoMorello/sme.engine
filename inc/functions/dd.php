e2f31eeee3150f6565f4878cc351ff55769a45f1
U-JUSTSERGEY\Sergey <sergomorello@yandex.ru>
2021-08-23 15:27:31 +0300

    Исправил получение url маршрута в видах

--- inc/functions/dd.php
+++ inc/functions/dd.php
@@ -3,11 +3,13 @@ function dd($data) {
 	$replacer = function($data) {
 		return str_replace("  ","#",str_replace("\n","<br>\r\n",print_r($data,true)));
 	};
-	echo $replacer($data);
-	echo "<h3>OTHER:</h3>";
-	echo "<h4>GET</h4>";
+	echo "<div style='color:#353535;padding: 10px;font-size:15px;border-bottom:1px dotted #212121;'>".$replacer($data)."</div>";
+	echo "<div style='color:#727272;font-size:12px;'>";
+	echo "<h3 style='color:#545454;'>OTHER:</h3>";
+	echo "<h4 style='color:#545454;'>GET</h4>";
 	echo $replacer($_GET);
-	echo "<h4>POST</h4>";
+	echo "<h4 style='color:#545454;'>POST</h4>";
 	echo $replacer($_POST);
+	echo "</div>";
 	die();
 }
--- inc/functions/route.php
+++ inc/functions/route.php
@@ -3,7 +3,7 @@ function route($name,$props=[]) {
 	$replaceVars = function($url,$vars=[]) {
 		$numVars = count($vars);
 		$i = 0;
-		$url = preg_replace_callback('/\{([0-9A-Za-z]{0,})\}/isU',function($var) use (&$vars,&$i) {
+		$url = preg_replace_callback('/\{([0-9A-Za-z?]{0,})\}/isU',function($var) use (&$vars,&$i) {
 			++$i;
 			return $vars[$i-1];
 		},$url);
