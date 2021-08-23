faf8553215e1c0ab4abd91a1d6f5bb4a9e3806d7
U-JUSTSERGEY\Sergey <sergomorello@yandex.ru>
2021-08-23 15:12:59 +0300

    в маршруты добавил не обязательные агрументы

--- inc/route.php
+++ inc/route.php
@@ -7,14 +7,17 @@ class route extends core {
 	}
 	protected function getRoute() {
 		$routes = self::$routes;
-		$urlMath = function($url) {
+		$allowChars = '0-9A-Za-z';
+		$urlMath = function($url) use (&$allowChars) {
 			return preg_replace([
 				'`/`is',
-				'`{(.*)}`isU'
+				'`\{[0-9A-Za-z]{0,10}\}`isU',
+				'`\\\/\{[0-9A-Za-z]{0,10}\?\}`isU'
 			],
 			[
 				'\/',
-				'(.*)'
+				'(['.$allowChars.']{0,})',
+				'[\/|]{0,1}(['.$allowChars.']{0,})'
 			],$url);
 		};
 		if ($routes)
@@ -22,11 +25,11 @@ class route extends core {
 				if ($this->getUrl()==$route['url'])
 					return $route;
 				//Определяем нужный маршрут
-				if ($route['url']!="/" && preg_match('/\s'.$urlMath($route['url']).'\s/is', ' '.$this->getUrl().' ', $matchUrl)) {
+				if ($route['url']!="/" && preg_match('/\s'.$urlMath($route['url']).'[\/|]{0,}\s/is', ' '.$this->getUrl().' ', $matchUrl)) {
 					//Получаем названия переменных из маршрута
 					if (preg_match_all("/\{(.*)\}/isU", $route['url'], $matchVars)) {
 						foreach($matchVars[1] as $key=>$varName)
-							$route['props'][$varName] = $matchUrl[$key+1];
+							$route['props'][str_replace('?','',$varName)] = $matchUrl[$key+1];
 						self::$props = $route['props'];
 					}
 					return $route;
