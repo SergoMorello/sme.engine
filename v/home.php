517ecdc7f179bfd0a330aff66e498222721f22dd
U-JUSTSERGEY\Sergey <sergomorello@yandex.ru>
2021-08-26 16:38:38 +0300

    Переделал главную страницу

--- v/home.php
+++ v/home.php
@@ -1,3 +1,23 @@
-<h1>SME ENGINE</h1>
-<h3>simple mvc engine</h3>
-<div>{{ $message }} </div>
+@extends('lay.html')
+
+@section('title','SME ENGINE')
+
+@section('content')
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="#">SME</a>
+		</div>
+	</nav>
+	<div class="row">
+		<div class="w-50 mx-auto">
+			<div class="card">
+				<div class="card-body">
+					<h5 class="card-title">SME Engine</h5>
+					<h6 class="card-subtitle mb-2 text-muted">simple mvc engine</h6>
+					<p class="card-text">Это нечто похожее на laravel но намного быстрее и проще</p>
+					<a href="https://github.com/SergoMorello/sme.engine" class="card-link">Github</a>
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection
