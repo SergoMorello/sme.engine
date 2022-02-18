371da3183e2dc0a7992e05df747a62bd62946e1a
unknown <sergomorello@yandex.ru>
2022-02-18 16:59:18 +0300

    Начал делать кеширование .env

--- engine/core/env.php
+++ engine/core/env.php
@@ -7,25 +7,31 @@ class env extends core {
 	public static function init() {
 		if (!file_exists(ROOT.'.env'))
 			die('.env not found');
-		
-		if ($file = file_get_contents(ROOT.'.env')) {
-			$list = explode(PHP_EOL,$file);
-			$arrCfg = [];
-			foreach($list as $li) {
-				if ((isset($li[0]) && $li[0]=='#') || !$li)
-					continue;
-				$key = NULL;
-				$value = NULL;
-				$it_li = explode('=',$li);
-				if (count($it_li)==2) {
-					list($key,$value) = $it_li;
-					$value = trim($value);
-					$vall = strtolower($value);
-					if ($vall=='true' || $vall=='false')
-						$value = $vall=='true' ? true : false;
+
+		if (!cache::has('__env') && !config('app.debug')) {
+
+			if ($file = file_get_contents(ROOT.'.env')) {
+				$list = explode(PHP_EOL,$file);
+				$arrCfg = [];
+				foreach($list as $li) {
+					if ((isset($li[0]) && $li[0]=='#') || !$li)
+						continue;
+					$key = NULL;
+					$value = NULL;
+					$it_li = explode('=',$li);
+					if (count($it_li)==2) {
+						list($key,$value) = $it_li;
+						$value = trim($value);
+						$vall = strtolower($value);
+						if ($vall=='true' || $vall=='false')
+							$value = $vall=='true' ? true : false;
+					}
+					self::$env[$key] = $value;
 				}
-				self::$env[$key] = $value;
+				cache::put('__env', self::$env);
 			}
+		}else{
+			self::$env = cache::get('__env');
 		}
 	}
 
